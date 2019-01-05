a408502df5d9debf9fdb011bb35b1bdcee6f969f
Tad Fisher <tadfisher@gmail.com>
2019-01-05 13:59:45 -0800

    profiles/gnome: Set pinentry-gnome3 as gpg-agent pinentry program

--- home/profiles/gnome.nix
+++ home/profiles/gnome.nix
@@ -1,9 +1,11 @@
 { config, lib, pkgs, ... }:
 
+with lib;
+
 {
   options.profiles.gnome.enable = lib.mkEnableOption "gnome desktop";
 
-  config = lib.mkIf config.profiles.gnome.enable {
+  config = mkIf config.profiles.gnome.enable {
     home.packages = with pkgs; [
       gnome3.gnome-boxes
       virtmanager
@@ -11,6 +13,10 @@
 
     # Prevent clobbering SSH_AUTH_SOCK
     pam.sessionVariables.GSM_SKIP_SSH_AGENT_WORKAROUND = "1";
+
+    services.gpg-agent.extraConfig = ''
+      pinentry-program ${pkgs.pinentry_gnome}/bin/pinentry-gnome3
+    '';
 
     # Disable gnome-keyring ssh-agent
     xdg.configFile."autostart/gnome-keyring-ssh.desktop".text = ''
