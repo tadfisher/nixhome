b9684d50e26b0424e7bf29e7e4b2771e9adcc9aa
Tad Fisher <tadfisher@gmail.com>
2019-10-10 16:15:49 -0700

    Fix missing xdg-mime associations

--- home/misc/xdg.nix
+++ home/misc/xdg.nix
@@ -1,4 +1,6 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
+
+with lib;
 
 let
   inherit (config.home) homeDirectory;
@@ -14,5 +16,15 @@ in {
     XDG_PUBLICSHARE_DIR="${homeDirectory}/public"
     XDG_TEMPLATES_DIR="${configHome}/templates"
     XDG_VIDEOS_DIR="${homeDirectory}/media/video"
+  '';
+
+  home.extraProfileCommands = ''
+    if [ -w $out/share/mime ] && [ -d $out/share/mime/packages ]; then
+      XDG_DATA_DIRS=$out/share PKGSYSTEM_ENABLE_FSYNC=0 ${pkgs.buildPackages.shared-mime-info}/bin/update-mime-database -V $out/share/mime > /dev/null
+    fi
+
+    if [ -w "$out/share/applications" ] ; then
+      ${pkgs.desktop-file-utils}/bin/update-desktop-database $out/share/applications
+    fi
   '';
 }
